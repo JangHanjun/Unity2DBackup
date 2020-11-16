68feb5da67178fdc489517ff084f980d3a98d137
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-16 22:26:15 +0900

    2020.11.16
    몬스터 아이템 드롭 구현

--- Assets/Script/EnemyDamaged.cs
+++ Assets/Script/EnemyDamaged.cs
@@ -10,6 +10,8 @@ public class EnemyDamaged : MonoBehaviour
     Rigidbody2D rigid;
     BoxCollider2D boxCollider2D;
     SpriteRenderer spriteRenderer;
+    bool isDrop;                        // 인스펙터를 통해 아이템 드랍 유무를 결정해줌
+    public GameObject dropItem;
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
@@ -21,6 +23,14 @@ public class EnemyDamaged : MonoBehaviour
             spriteRenderer.color = new Color(1, 1, 1, 0.4f);
             boxCollider2D.enabled = false;
             Destroy(gameObject, 2f);
+            ItemDrop();
+        }
+    }
+
+    void ItemDrop(){
+        int dropPercent = Random.Range(1,101);
+        if(dropPercent < 30){    // 뒤 숫자가 확률이다
+            Instantiate(dropItem, this.gameObject.transform.position, transform.rotation);
         }
     }
 }
