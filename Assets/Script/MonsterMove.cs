12ff161530eb3ec566f244cf033100d8c6a12aa9
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-18 18:08:24 +0900

    2020.11.18
    추적하는 몬스터 구현

--- Assets/Script/MonsterMove.cs
+++ Assets/Script/MonsterMove.cs
@@ -2,37 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterMove : MonoBehaviour
-{
+public class MonsterMove : MonoBehaviour {
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
     public int moveDir;    // Moving direction, Random
-    public int moveF;
 
-    void Awake()
-    {
+    void Awake() {
         rigid = GetComponent<Rigidbody2D>();
-        monsterAI();
-        // todo : flip
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    void FixedUpdate()
-    {
-        rigid.velocity = new Vector2(moveDir, rigid.velocity.y);   // no jump monster
-
-        // raycast for detect fall
-        Vector2 monsterVec = new Vector2(rigid.position.x + moveDir, rigid.position.y);
-        Debug.DrawRay(monsterVec, Vector3.down, new Color(1, 0, 0));   // for debug
-        RaycastHit2D raycastHit = Physics2D.Raycast(monsterVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
-        if(raycastHit.collider == null) {
-            if (raycastHit.distance < 0.5f) {
-                moveDir = -moveDir;  // Change direction
-            }
+    void Start(){
+        StartCoroutine("monsterAI");
+    }
+    void Update() {
+        if (rigid.velocity.x > 0.1f) {
+            spriteRenderer.flipX = true;
+        } else {
+            spriteRenderer.flipX = false;
         }
     }
 
-    void monsterAI() {
-        moveDir = Random.Range(-1, 2);   // -1<= ranNum <2
-        // change frequency,  2 can be random num like moveDir   or    public float to see in inspector
-        Invoke("monsterAI", moveF);
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(moveDir, rigid.velocity.y);   // no jump monster
     }
+
+    IEnumerator monsterAI() {
+        moveDir = Random.Range(-1, 2);   // -1<= ranNum <2
+        yield return new WaitForSeconds(5f);
+        StartCoroutine("monsterAI");
+    }
+
+    public void startMove(){
+        StartCoroutine("monsterAI");
+    }
+
+    public void stopMove(){
+        StopCoroutine("monsterAI");
+    }
+
 }
